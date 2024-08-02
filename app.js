15bb390c1fec8ba6ae026eda035db6ae16ec15c2
Oscar Qiu <henry22656253b@gmail.com>
2024-08-02 14:18:21 +0800

    添加 line notify 功能, 新增 排程, 環境讀取的依賴項目
    1. 將 token 建立在外部文件, 透過讀取方式避免機敏資料暴露
    
    2. GCP 機器預設為 UTC, 因TW 時區為 UTC+8 , Cron 設定需 - 8

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const redis = require('redis');
 const bodyParser = require('body-parser');
+const cron = require('node-cron');
+const axios = require('axios');
+require('dotenv').config();  // 讀取 .env 文件
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +13,42 @@ const client = redis.createClient({
 });
 
 client.connect().catch(console.error);
+
+// 從環境變量中讀取 LINE Notify token
+const lineNotifyToken = process.env.LINE_NOTIFY_TOKEN;
+
+// Function to send notification
+const sendLineNotify = async (message) => {
+  try {
+    await axios.post('https://notify-api.line.me/api/notify', 
+      `message=${encodeURIComponent(message)}`, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': `Bearer ${lineNotifyToken}`
+        }
+      }
+    );
+    console.log('Notification sent');
+  } catch (error) {
+    console.error('Error sending notification:', error);
+  }
+};
+
+// Schedule a task to run at 00:45 UTC (08:45 Taiwan time) from Monday to Friday
+cron.schedule('45 0 * * 1-5', () => {
+  sendLineNotify('\n別忘了到寬聯網站打上班卡~\nhttps://eip.kli.com.tw');
+});
+
+// Schedule a task to run at 00:45 UTC (08:45 Taiwan time) from Monday to Friday
+cron.schedule('5 10 * * 1-5', () => {
+  sendLineNotify('\n別忘了到寬聯網站打下班卡~ 謝謝\nhttps://eip.kli.com.tw');
+});
+
+app.post('/sendLineNotify/:word', (req, res) => {
+  const { word } = req.params;
+  sendLineNotify(`You just said ${word} `);
+  sendLineNotify('\n別忘了到寬聯網站打下班卡~ 謝謝\nhttps://eip.kli.com.tw');
+});
 
 app.post('/data', async (req, res) => {
   try {
@@ -36,10 +75,7 @@ app.get('/say/:word', (req, res) => {
   res.send(`You just said ${word} `);
 });
 
-
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
